861ecc73daaccd8cc73323cbc669594b6ce879f5
Devon <devonhurteau@gmail.com>
2025-10-13 13:48:00 -0600

    Added celery notification system

---
+++ notification.py
@@ -0,0 +1,33 @@
+from datetime import timedelta
+from celery import Celery
+from runner import EsportsRunner
+
+celery_app = Celery(
+    "notify_user_celery",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/0",
+)
+
+celery_app.conf.update(
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+)
+
+celery_app.conf.beat_schedule = {
+    "send_notifications": {
+        "task": "notification.notify_user",
+        "schedule": timedelta(seconds=45),
+    },
+}
+
+@celery_app.task(name="notification.notify_user")
+def notify_user():
+    runner = EsportsRunner()
+    runner.run_bot()
+
+
+
+
+# celery -A notification worker --loglevel=info --pool=solo
+# celery -A notification beat --loglevel=info
